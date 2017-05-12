4b134f695d2222f20619553f2adbe9ac2b3126f7
Shreve-La <shrevela@gmail.com>
2017-05-12 13:48:07 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by swcl on 2017-05-12.
+//  Copyright © 2017 Shreve LaFramenta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
